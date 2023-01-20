b2f9ec068570feea859fcc682df2950700a3fd27
jwong65 <jason.wong6507@gmail.com>
2023-01-19 21:17:55 -0500

    Added the ColorBlock.js

---
+++ react-rainbow/src/ColorBlock.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ColorBlock(property){
+    return(
+        <div className='colorBlock' style={{'backgroundColor': property.color}}>
+            <p>This is {property.color}</p>
+
+        </div>
+    )
+}
